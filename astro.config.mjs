57bc3736118a1f15b41fcc821395110df3db43d8
Cristian Adán <salteadorneo@users.noreply.github.com>
2024-11-10 13:11:52 +0000

    Update configuration for Deno deployment and version bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import deno from '@deno/astro-adapter';
 
 export default defineConfig({
+  output: 'server',
+  adapter: deno(),
   integrations: [tailwind()]
 });
