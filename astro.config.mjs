fd4e23abed12368937bf981d83fba969f1ba2b05
Cristian Adán <hola@salteadorneo.dev>
2024-11-16 02:12:30 +0100

    Update Astro configuration and dependencies for improved performance and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import deno from '@deno/astro-adapter';
 
 export default defineConfig({
-  output: 'server',
-  adapter: deno(),
-  integrations: [tailwind()]
+  site: "https://rugbytorrejon.com",
+  integrations: [tailwind()],
 });
